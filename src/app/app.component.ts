5da5608150535b521c317d860849128de4cf0b41
Stephen Charette <scharette@solutionstreet.com>
2024-03-01 18:04:31 -0500

    FIrst pass at pass screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,29 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatOption } from '@angular/material/core';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    RouterOutlet,
+    MatFormField,
+    MatLabel,
+    MatOption,
+    MatSelectModule,
+    MatSlideToggleModule,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'ng-pass';
