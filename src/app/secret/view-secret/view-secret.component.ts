f51f1ea9e82f1ebb9bda032fdc4b202f2d9502aa
Stephen Charette <scharette@solutionstreet.com>
2024-03-03 13:57:53 -0500

    Pretty secret copy to clipboard

--- src/app/secret/view-secret/view-secret.component.ts
+++ src/app/secret/view-secret/view-secret.component.ts
@@ -2,8 +2,10 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 // Services
+import { CopyToClipboardService } from '../../services/copy-to-clipboard/copy-to-clipboard.service';
 import { RoutingService } from '../../services/routing/routing.service';
 import { RetrieveSecretService } from '../../services/retrieve-secret/retrieve-secret.service';
+import { SnackBarService } from '../../services/snack-bar/snack-bar.service';
 
 // MaterialUI
 import { FormsModule } from '@angular/forms';
@@ -11,6 +13,8 @@ import { MatFormField, MatLabel } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatRippleModule } from '@angular/material/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-view-secret',
@@ -23,6 +27,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
     MatButtonModule,
     MatFormFieldModule,
     CommonModule,
+    MatRippleModule,
+    MatIconModule,
   ],
   templateUrl: './view-secret.component.html',
   styleUrl: './view-secret.component.css',
@@ -35,13 +41,22 @@ export class ViewSecretComponent {
   status: undefined | number;
   constructor(
     private routingService: RoutingService,
-    private retrieveSecretService: RetrieveSecretService
+    private retrieveSecretService: RetrieveSecretService,
+    private copyToClipboardService: CopyToClipboardService,
+    private snackBarService: SnackBarService
   ) {}
 
   ngOnInit() {
     this.routingService.getQueryParam('key').subscribe((key) => {
       this.key = key;
     });
+  }
+
+  copySecretToClipboard() {
+    if(this.secret) {
+      this.copyToClipboardService.copyToClipboard(this.secret);
+      this.snackBarService.openSnackBar('Secret copied to clipboard', 'Close', { duration: 3000 });
+    }
   }
 
   toggleRevealDisabled(toggle: boolean) {
@@ -56,12 +71,11 @@ export class ViewSecretComponent {
         this.toggleRevealDisabled(true);
       },
       error: (e) => {
-        if(e.status === 404) {
+        if (e.status === 404) {
           this.toggleRevealDisabled(true);
         }
         this.status = e.status;
-      },
-      complete: () => {},
+      }
     });
   }
 }
